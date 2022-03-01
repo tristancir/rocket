390f068de87a4c2be719e81cd3df183e40b06622
ryan teixeira <ryantxr@gmail.com>
2022-03-01 12:44:40 -0500

    migration

---
+++ webapp/database/migrations/2022_03_01_173156_add_removed_column.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddRemovedColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('channel_post', function (Blueprint $table) {
+            $table->tinyInteger('is_removed')->nullable()->default(null)
+                ->after('channel_id');
+            $table->smallInteger('http_status')
+                ->unsigned()
+                ->nullable()
+                ->default(null)->after('is_removed');
+            $table->string('error_msg', 400)->nullable()->default(null)->after('http_status');
+            $table->datetime('checked_at')->nullable()->default(null)->after('error_msg');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('channel_post', function (Blueprint $table) {
+            $table->dropColumn('is_removed');
+            $table->dropColumn('http_status');
+            $table->dropColumn('error_msg');
+            $table->dropColumn('checned_at');
+        });
+    }
+}
